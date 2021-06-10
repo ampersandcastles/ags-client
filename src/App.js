2163da8335fdfe6cf196d4f327d8087b1327846d
Tristan Smith <tristan.smith@gmail.com>
2021-06-10 14:15:27 -0400

    updated navbar and menu icon - added screenshot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import hopLogo from "./assets/hop.png";
+import React from "react";
+import { Button } from "@material-ui/core";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
+import hopLogo from "./assets/hop-no-text.png";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div id="mainDiv">
+      <div className="nav">
+        <Button color="primary">
+          <MenuIcon id="menu" />
+        </Button>
+        <a href="/">
+          <img id="hopLogo" src={hopLogo} alt="House of Pain" />
+        </a>
+      </div>
+      <div className="body">
+        <p>Trial and error.</p>
+      </div>
     </div>
   );
 }
