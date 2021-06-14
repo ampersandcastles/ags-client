03d6f2863285d653b25df946ec4af576f691b3c3
Tristan Smith <tristan.smith@gmail.com>
2021-06-14 11:51:41 -0400

    wow, these cards were much easier

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 import Body from "./components/Body/Body";
 import Navbar from "./components/Navbar/Navbar";
+import Items from "./components/Popular-Items/Items";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Body />
+      <Items />
     </div>
   );
 }
