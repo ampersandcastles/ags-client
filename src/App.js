aa79abcde9454245367b6f0b50864fd974b0c176
Tristan Smith <tristan.smith@gmail.com>
2021-06-14 10:49:28 -0400

    navbar moved to own component. body started

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { Button } from "@material-ui/core";
 import "./App.css";
-import hopLogo from "./assets/hop-no-text.png";
-import MenuIcon from "@material-ui/icons/Menu";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
+import Body from "./components/Body/Body";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div id="mainDiv">
-      <div className="nav">
-        <Button color="primary">
-          <MenuIcon id="menu" />
-        </Button>
-        <a href="/">
-          <img id="hopLogo" src={hopLogo} alt="House of Pain" />
-        </a>
-        <Button color="primary">
-          <ShoppingCartIcon id="shopping" />
-        </Button>
-      </div>
-      <div className="body">
-        <p>Trial and error.</p>
-      </div>
+    <div>
+      <Navbar />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import hopLogo from "../../assets/hop-no-text.png";
+import MenuIcon from "@material-ui/icons/Menu";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import "./Navbar.css";
+import Center from "react-center";
+import { Button } from "@material-ui/core";
+
+const Navbar = (props) => {
+  return (
+    <div id="mainDiv">
+      <div className="nav">
+        <Button color="primary">
+          <MenuIcon id="menu" />
+        </Button>
+        <a href="/">
+          <img id="hopLogo" src={hopLogo} alt="House of Pain" />
+        </a>
+        <Button color="primary">
+          <ShoppingCartIcon id="shopping" />
+        </Button>
+      </div>
+      <div className="body"></div>
+    </div>
+  );
+};
+
+export default Navbar;
