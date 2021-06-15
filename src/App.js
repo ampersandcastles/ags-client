f4d2580a5a7301b80d9cb24d085a1d8cde0e97f4
Amy Melangton <73564773+armelangton@users.noreply.github.com>
2021-06-15 15:16:04 -0400

    Revert "Tristan"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
-import "./App.css";
-
-// import Body from "./components/Body/Body";
-// import Navbar from "./components/Navbar/Navbar";
-// import Items from "./components/Popular-Items/Items";
-import Register from "./components/Register/Register";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="main">
-      {/* <Navbar />
-      <Body />
-      <Items /> */}
-      <Register />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
