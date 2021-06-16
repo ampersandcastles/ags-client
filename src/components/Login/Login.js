cf7196c0540875ab4eee755948465e44b08e6d42
Tristan <tristan.smith@gmail.com>
2021-06-16 11:54:08 -0400

    css change to login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,49 +1,55 @@
-import React, {useState} from 'react';
-import {Form, FormGroup, Label, Input, Button} from 'reactstrap'; //1
-import APIURL from '../../helpers/Environment';
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap"; //1
+import APIURL from "../../helpers/Environment";
+import "./Login.css";
 
 const Login = (props) => {
-    const [email, setEmail] = useState(''); //2
-    const [password, setPassword] = useState(''); //2
+  const [email, setEmail] = useState(""); //2
+  const [password, setPassword] = useState(""); //2
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`${APIURL}/user/login`, {
+      method: "POST",
+      body: JSON.stringify({ email: email, password: password }),
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        props.updateToken(data.token);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        fetch(`${APIURL}/user/login`, {
-            method: 'POST',
-            body: JSON.stringify({email: email, password: password}),
-            headers: new Headers ({
-                'Content-Type': 'application/json'
-            })
-        }) 
-        .then(response => response.json())
-        .then((data) => {
-
-            console.log(data);
-            props.updateToken(data.token)
-        } 
-            )
-        .catch(err => console.log(err))
-        
-    }
-
-
-    return(
-        <div>
-            <h1>Login</h1>
-            <Form onSubmit={handleSubmit}>
-                <FormGroup>
-                    <Label htmlFor="email">Email</Label> 
-                    <Input onChange={(e)=> setEmail(e.target.value)} name="email" value={email}/> {/* 3 */}
-                </FormGroup>
-                <FormGroup>
-                    <Label htmlFor="password">Password</Label>
-                    <Input onChange={(e) => setPassword(e.target.value)} name="password" value={password} /> {/* 3 */}
-                </FormGroup>
-                <Button type="submit">Login</Button>
-            </Form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <FormGroup>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            value={email}
+          />{" "}
+          {/* 3 */}
+        </FormGroup>
+        <FormGroup>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            value={password}
+          />{" "}
+          {/* 3 */}
+        </FormGroup>
+        <Button type="submit">Login</Button>
+      </Form>
+    </div>
+  );
+};
 
 export default Login;
